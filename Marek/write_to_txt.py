ba401e75a50f68d2504e0b2575b400d42e8d8398
a <markusus123765@gmail.com>
2024-11-07 19:26:34 +0100

    zmieniono nazwe

---
+++ Marek/write_to_txt.py
@@ -0,0 +1,13 @@
+import os
+
+if not os.path.exists('./output'):
+    os.makedirs('./output')
+
+text = "Lubie pociągi"
+combine_text = ""
+for i in range(2137):
+    combine_text += f"{i} {text} \n"
+file_name = "output/tescik.txt"
+with open(file_name, 'w', encoding='utf-8') as file:
+    file.write(combine_text)
+print(f"Text was saved to a file {file_name}.")

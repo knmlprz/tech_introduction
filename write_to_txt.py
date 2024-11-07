56242ac2e413878dde6dfff32c62c9ca0898ff53
DamianKobyliński <damiankob12@gmail.com>
2024-11-07 14:53:09 +0000

    TestTest

--- write_to_txt.py
+++ write_to_txt.py
@@ -1,8 +1,9 @@
-text = "Daniel przeszedł szkolenie techniczne"
+
+text = "Test Test"
 combine_text = ""
 for i in range(2137):
     combine_text += f"{i} {text} \n"
-file_name = "output/daniel_k.txt"
+file_name = "output/test.txt"
 with open(file_name, 'w', encoding='utf-8') as file:
     file.write(combine_text)
 print(f"Text was saved to a file {file_name}.")
